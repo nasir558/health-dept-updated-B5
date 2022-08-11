7f5bbe20b6e7e713bdb913a412904c503cb6fba7
Abdul Nasir <abdul.nasir55531@gmail.com>
2022-08-11 12:19:56 +0500

    added new page for image gallery

--- assets/js/inner-pages-custom.js
+++ assets/js/inner-pages-custom.js
@@ -2,5 +2,5 @@ $(document).ready(function ($) {
     $('#toggleGreyLayout').click(function (e) {
         e.preventDefault();
         $('body').toggleClass('greyScaleLayout');
-    })
+    });
 });
